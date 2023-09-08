6b6b939f69ec569c5659e798f3c8b637989aeade
Amanda <aamandaallinger@outlook.com>
2023-09-08 12:03:23 +0200

    -

--- server/productRouter.js
+++ server/productRouter.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const productRouter = express.Router();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-productRouter.get("/", (req, res) => {
-    res.send("Hello")
-});
+productRouter.get('/api/products', async (req, res) => {
+    try {
+      const products = await stripe.prices.list({ active: true });
+      res.json(products.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Kunde inte hämta produkter.' });
+    }
+  });
 
 module.exports = productRouter;
