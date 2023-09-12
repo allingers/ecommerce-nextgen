1dbc874502a356fa103db6efdc734278f2a81e8f
Amanda <aamandaallinger@outlook.com>
2023-09-12 10:25:07 +0200

    productTypes

--- client/src/Intefaces/productTypes.ts
+++ client/src/Intefaces/productTypes.ts
@@ -1,8 +1,9 @@
 // I en separat fil, t.ex. productTypes.ts
 export interface Product {
+    product: any;
     id: string;
     name: string;
     price: number;
     image: string;
+    quantity: number; // Lägg till quantity som en valfri egenskap
   }
-  
